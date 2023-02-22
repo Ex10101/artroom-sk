32e28f04ea8953c010b9ec3df7238a469d76b456
Ex10101 <sergeydavidovich12@gmail.com>
2023-02-22 14:24:44 +0100

    Refactor + CRU
    Code refactoring and creating a CRUD withoud DELETE

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const dbUrl = 'mongodb://127.0.0.1:27017/artroom';
 const mongoose = require('mongoose');
 const Project = require('./models/project');
 
-app.use(express.urlencoded({ extended: true }));
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
+// Connection
 mongoose.connect(dbUrl)
   .then(() => {
     console.log('Connected to database');
@@ -21,6 +14,52 @@ mongoose.connect(dbUrl)
   });
 
 const port = process.env.PORT || 3000;
+
+
+
+// Configuration
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+
+// Project CRUD
+app.get('/projects', async (req, res) => {
+  const projects = await Project.find({});
+  res.render('projects/index', { projects });
+})
+
+
+app.get('/projects/new', (req, res) => {
+  res.render('projects/new');
+})
+app.post('/projects', async (req, res) => {
+  const project = new Project(req.body.project);
+  await project.save();
+  console.log(project);
+})
+
+app.get('/projects/:id', async (req, res) => {
+  const project = await Project.findById(req.params.id);
+  console.log(project);
+  res.render('projects/show', {project});
+})
+
+app.get('/projects/:id/edit', async (req, res) => {
+  const project = await Project.findById(req.params.id);
+  res.render('projects/edit', { project })
+})
+app.post('/projects/:id/update', async (req, res) => {
+  const project = await Project.findByIdAndUpdate(req.params.id, { ...req.body.project });
+  await project.save();
+  console.log(project);
+  res.redirect('/projects');
+})
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- models/project.js
+++ models/project.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  images: [{ type: String, required: true }]
+  image: { type: String, required: true }
 });
 
 const Project = mongoose.model('Project', projectSchema);
