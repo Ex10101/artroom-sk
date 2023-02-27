32dc1a9d06c2b67e11397b355f2bccf6593c81b0
sarcasacras <dewaleguitar@gmail.com>
2023-02-27 12:45:35 +0100

    Created controls with arrow keys

--- public/scripts/gallery.js
+++ public/scripts/gallery.js
@@ -1,3 +1,6 @@
+let currentImageIndex = 0; // Индекс текущей выбранной картинки
+let totalImages = 0; // Общее количество картинок в галерее
+
 window.onload = function () {
     const images = document.querySelectorAll('.img-gallery img');
     images.forEach(image => {
@@ -6,6 +9,10 @@ window.onload = function () {
 }
 
 function showLightbox(event) {
+    const images = document.querySelectorAll('.img-gallery img');
+    totalImages = images.length;
+    currentImageIndex = Array.from(images).indexOf(event.target); // Получаем индекс текущей выбранной картинки
+
     const lightbox = document.createElement('div');
     lightbox.classList.add('lightbox');
 
@@ -16,10 +23,37 @@ function showLightbox(event) {
     document.body.appendChild(lightbox);
 
     lightbox.addEventListener('click', hideLightbox);
+
+    // Добавляем обработчики событий на клавиши влево и вправо
+    document.addEventListener('keydown', handleKeyDown);
+}
+
+function handleKeyDown(event) {
+    if (event.key === 'ArrowLeft') {
+        currentImageIndex--;
+        if (currentImageIndex < 0) {
+            currentImageIndex = totalImages - 1; // Обрабатываем случай, когда достигнута первая картинка
+        }
+        updateLightboxImage();
+    } else if (event.key === 'ArrowRight') {
+        currentImageIndex++;
+        if (currentImageIndex >= totalImages) {
+            currentImageIndex = 0; // Обрабатываем случай, когда достигнута последняя картинка
+        }
+        updateLightboxImage();
+    }
+}
+
+function updateLightboxImage() {
+    const lightbox = document.querySelector('.lightbox');
+    const img = document.querySelector('.lightbox img');
+    const images = document.querySelectorAll('.img-gallery img');
+    img.src = images[currentImageIndex].src;
 }
 
 function hideLightbox(event) {
     if (event.target.classList.contains('lightbox')) {
+        document.removeEventListener('keydown', handleKeyDown);
         event.target.remove();
     }
 }
