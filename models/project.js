7f8b9ae2ce2886de51779d827f60b0cbea1f3a65
sarcasacras <dewaleguitar@gmail.com>
2023-03-01 13:38:18 +0100

    Updated model

--- models/project.js
+++ models/project.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  type: { 
+    type: String, 
+    enum: ['Visualisation', 'Architecture', 'Design'], // Accepts only 1 option out of these 3
+    required: true 
+  },
   images: [{type: String}]
 });
+
+projectSchema.index({ type: 1 }); // In case of sorting by type this will improve the performance
 
 const Project = mongoose.model('Project', projectSchema);
 
 module.exports = Project;
+
