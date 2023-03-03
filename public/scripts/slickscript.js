b0623b03261fa5c8e3424ef645d34b1766263af3
sarcasacras <dewaleguitar@gmail.com>
2023-03-03 10:08:17 +0100

    some fixes
    1. no slick
    2. fonts (they are in footer.css)
    3. some responsive fixes
    4. contacts link in navbar

--- public/scripts/slickscript.js
+++ public/scripts/slickscript.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        arrows: false
-    });
-});
+// $(document).ready(function(){
+//     $('.carousel').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         adaptiveHeight: true,
+//         // responsive: [
+//         //     {
+//         //         breakpoint: 992,
+//         //         settings: {
+//         //             slidesToShow: 1,
+//         //             slidesToScroll: 1
+//         //         }
+//         //     },
+//         //     {
+//         //         breakpoint: 768,
+//         //         settings: {
+//         //             slidesToShow: 1,
+//         //             slidesToScroll: 1,
+//         //             dots: false
+//         //         }
+//         //     }
+//         // ]
+//     });
+// });
