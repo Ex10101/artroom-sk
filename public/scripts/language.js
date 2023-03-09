b1ed1f44d95f799760e5917e3381185f91146377
sarcasacras <dewaleguitar@gmail.com>
2023-03-09 08:50:40 +0100

    Bug fix with language

--- public/scripts/language.js
+++ public/scripts/language.js
@@ -1,17 +1,17 @@
 const languageSelect = document.getElementById("language");
 
-languageSelect.addEventListener("change", function () {
+languageSelect.addEventListener("change", function() {
   const selectedValue = languageSelect.value;
   const currentUrl = window.location.href;
 
   if (selectedValue === "sk") {
-    if (!currentUrl.includes("/sk/")) {
-      const newUrl = currentUrl.replace(/(^\w+:|^)\/\/([^\/]+)/, `$1//$2/sk/`);
+    if (!currentUrl.includes("/sk")) {
+      const newUrl = currentUrl.replace(/(^\w+:|^)\/\/([^\/]+)/, `$1//$2/sk`);
       window.location.href = newUrl;
     }
   } else {
-    if (currentUrl.includes("/sk/")) {
-      const newUrl = currentUrl.replace("/sk/", "/");
+    if (currentUrl.includes("/sk")) {
+      const newUrl = currentUrl.replace("/sk", "");
       window.location.href = newUrl;
     }
   }
