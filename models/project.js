d4c6e86c68b181091958d755a2a79a597983e3a4
sarcasacras <dewaleguitar@gmail.com>
2023-03-19 01:14:59 +0100

    Добавлены переводы для проектов
    плюс некоторые фиксы, в том числе новая модель, и новая форма

--- models/project.js
+++ models/project.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  slovakDescription: { type: String, required: false },
   type: { 
     type: String, 
-    enum: ['Visualisation', 'Architecture', 'Design'], // Accepts only 1 option out of these 3
+    enum: ['Visualisation', 'Architecture', 'Design'], 
     required: true 
   },
   images: [{type: String}]
 });
 
-projectSchema.index({ type: 1 }); // In case of sorting by type this will improve the performance
+projectSchema.index({ type: 1 }); 
 
 const Project = mongoose.model('Project', projectSchema);
 
