92d63d6c2ab2d377ea419844dc5fe89a8cc7482c
sarcasacras <dewaleguitar@gmail.com>
2023-03-20 02:40:34 +0100

    Йоу

--- models/project.js
+++ models/project.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  skName: { type: String, required: false},
   description: { type: String, required: true },
   slovakDescription: { type: String, required: false },
   shortDescription: { type: String, required: false },
